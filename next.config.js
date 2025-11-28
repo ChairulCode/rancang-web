610996aa5e3f04a2c61a2ea1fa57daab0eea0783
ChairulCode <chairularul010805@gmail.com>
2025-11-29 04:11:32 +0700

    ubah package

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
-const isProd = process.env.NODE_ENV === "production";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? "/nextly/" : "",
-  basePath: isProd ? "/nextly" : "",
   images: {
-    loader: "akamai",
-    path: "",
+    unoptimized: true,
   },
 };
 
