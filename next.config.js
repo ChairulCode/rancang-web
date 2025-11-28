2ad5a13a49b8b29c34f7a90a563fcc7efa05a3c1
ChairulCode <chairularul010805@gmail.com>
2025-11-29 04:10:06 +0700

    test

--- next.config.js
+++ next.config.js
@@ -1,17 +1,3 @@
-// module.exports = {
-//   i18n: {
-//     locales: ["en"],
-//     defaultLocale: "en",
-//   },
-//   target: "serverless",
-//   webpack: (config, { isServer }) => {
-//     if (!isServer) {
-//       config.resolve.fallback.fs = false;
-//     }
-//     return config;
-//   },
-// };
-
 const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
@@ -21,8 +7,6 @@ const nextConfig = {
   assetPrefix: isProd ? "/nextly/" : "",
   basePath: isProd ? "/nextly" : "",
   images: {
-    // The `unoptimized: true` option is not supported in your Next.js version.
-    // For `next export` to work with `next/image`, you must use a loader other than the default.
     loader: "akamai",
     path: "",
   },
